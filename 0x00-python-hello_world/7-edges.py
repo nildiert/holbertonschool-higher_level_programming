aaa85e56d90f9a8d4061bc9c1fc625bb130251fb
nildiert <niljordan23@gmail.com>
2019-04-29 17:28:42 +0000

    [FEATURE] Add the file 7, first and last letters of a string

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3, word_last_2, middle_word = word[0:3], word[-2:], word[1:-1]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
