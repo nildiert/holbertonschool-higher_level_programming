8db77ca6907082758d32ad2793295670f379af75
nildiert <niljordan23@gmail.com>
2019-04-29 20:30:47 +0000

    [FEATURE] Added the exercise 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Entry point
+ *
+ * @list: List that receives
+ * Return: Always 0 (Success)
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *same_1 = list;
+	listint_t *same_2 = list;
+
+	same_2 = same_2->next;
+	while ((same_1 != NULL && same_2 != NULL) || same_2->next != NULL)
+	{
+		if (same_2 == same_1)
+			return (1);
+		if (same_2->next != NULL)
+			same_2 = same_2->next->next;
+		else
+			return (0);
+		if (same_1->next != NULL)
+			same_1 = same_1->next;
+		else
+			return (0);
+	}
+	return (0);
+}
