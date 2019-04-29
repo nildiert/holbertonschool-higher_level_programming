a7cc894acd30dcf83f895a6f8ff483e93bf58baf
nildiert <niljordan23@gmail.com>
2019-04-29 20:42:11 +0000

    [FIX] Fixed the efficiency of file 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -15,17 +15,12 @@ int check_cycle(listint_t *list)
 	if (list != NULL)
 	{
 		same_2 = same_2->next;
-		while ((same_1 != NULL && same_2 != NULL) || same_2->next != NULL)
+		while ((same_1 != NULL && same_2 != NULL) && same_2->next != NULL)
 		{
 			if (same_2 == same_1)
 				return (1);
-			if (same_2->next != NULL && same_1->next != NULL)
-			{
-				same_2 = same_2->next->next;
-				same_1 = same_1->next;
-			}
-			else
-				return (0);
+			same_2 = same_2->next->next;
+			same_1 = same_1->next;
 		}
 	}
 	return (0);
