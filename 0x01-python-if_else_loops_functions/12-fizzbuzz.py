514f486dffa5ec0a500745d63d5c5a78cd5d9abb
nildiert <niljordan23@gmail.com>
2019-04-30 23:26:55 +0000

    [FEATURE] Added fizzBuzz function

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 3 != 0 and i % 5 != 0):
+            print("{:d} ".format(i), end='')
+        else:
+            print("{}".format("Fizz" if ((i % 3) == 0) else ""), end='')
+            print("{} ".format("Buzz" if ((i % 5) == 0) else ""), end='')
