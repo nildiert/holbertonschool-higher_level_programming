71b574980427deffca95aadbe6ec7c0e1719b5ef
nildiert <niljordan23@gmail.com>
2019-04-30 23:34:18 +0000

    [FIX] Fixed the spaces in fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 != 0 and i % 5 != 0):
-            print("{:d} ".format(i), end='')
+            print("{:d}".format(i), end=' ')
         else:
             print("{}".format("Fizz" if ((i % 3) == 0) else ""), end='')
             print("{}".format("Buzz" if ((i % 5) == 0) else ""), end=' ')
