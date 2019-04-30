c44e19ee984104ee20bf1e72d6d9866d8b49564c
nildiert <niljordan23@gmail.com>
2019-04-30 19:09:13 +0000

    [FIX] Fixed the pep8 style in file 1 and error in the numbers less than 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 if (number % 10) > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, number % 10))
+    print("Last digit of {:d} is {:d} ".format(number, number % 10))
+    print("and is greater than 5")
 elif (number % 10) == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, number % 10))
 else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, number % 10))
+    print("Last digit of {:d} is {:d} ".format(number, abs(number) % 10))
+    print("and is less than 6 and not 0")
