24eaae143abbaaff38ba37987c2af260085e833c
nildiert <niljordan23@gmail.com>
2019-05-01 01:13:53 +0000

    [FIX] Fixed efficiency in file 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,36 +1,6 @@
 #include "lists.h"
 
 /**
- * *add_nodeint - Print the list of a single list
- * @n: The variable of int type that receives
- * @head: The pointer of the list
- *
- * Return: The number of elements in the list
- */
-
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new = malloc(sizeof(head));
-	listint_t *copy = *head;
-
-	if (!new)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (!*head)
-	{
-		*head = new;
-		return (*head);
-	}
-	else
-	{
-		new->next = copy;
-		*head = new;
-	}
-	return (*head);
-}
-
-/**
  * *insert_node - adds a new node at the end of a listint_t list
  * @head: pointer to pointer of first node of listint_t list
  * @number: integer to be included in new node
@@ -50,14 +20,17 @@ listint_t *insert_node(listint_t **head, int number)
 	new->n = number;
 	if (!*head)
 	{
-
 		new->next = NULL;
 		*head = new;
 		return (new);
 	}
 	copy = *head;
 	if (number <= 0)
-		return (add_nodeint(head, number));
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	while (copy->n < (number))
 	{
 		aux = copy;
@@ -67,9 +40,6 @@ listint_t *insert_node(listint_t **head, int number)
 		copy = copy->next;
 	}
 	new->next = aux->next;
-	new->n = number;
 	aux->next = new;
 	return (new);
-
-
 }
