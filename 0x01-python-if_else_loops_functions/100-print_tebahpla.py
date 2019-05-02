5d7a64c25daae97b8de029b8c0b38362c8209790
nildiert <niljordan23@gmail.com>
2019-05-02 01:10:18 +0000

    [FIX] Fixed the requirements of the file 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for c in range(123, 96, -1):
-    if (c != 123):
+for c in range(ord('z'), ord('a') - 1, -1):
         print ("{}".format(chr(c)) if c % 2 == 0 else chr(c - 32), end="")
