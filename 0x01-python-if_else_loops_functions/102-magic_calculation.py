09c47501e0ca68618a30ae30c94d7ef57ec69132
nildiert <niljordan23@gmail.com>
2019-05-02 02:28:21 +0000

    [FEATURE] Added a magic binary function

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if (a < b):
+        return (c)
+    else:
+        if (c > b):
+            return (a + b)
+        else:
+            return ((a * b) - c)
