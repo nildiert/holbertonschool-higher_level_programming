38a6bf0902cf1c926009c38e974fa050deae0755
nildiert <niljordan23@gmail.com>
2019-05-02 02:53:06 +0000

    [FEATURE] Fixed space in file 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for c in range(ord('z'), ord('a') - 1, -1):
-        print ("{}".format(chr(c)) if c % 2 == 0 else chr(c - 32), end="")
+        print("{}".format(chr(c)) if c % 2 == 0 else chr(c - 32), end="")
