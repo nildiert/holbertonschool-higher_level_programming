39d93c18f8c446d58d662593f7ad76f05a82dd05
nildiert <niljordan23@gmail.com>
2019-05-03 02:14:45 +0000

    [FEATURE] Added function that prints arguments of a function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {}".format(i, sys.argv[i]))
+    if (len(sys.argv) - 1):
+        if (len(sys.argv) - 1 == 1):
+            print("{:d} argument:".format(len(sys.argv) - 1))
+        else:
+            print("{:d} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
