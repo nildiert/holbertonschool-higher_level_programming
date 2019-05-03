cf69e75e0d72152270e3c1fd8b05e3020fe28b72
nildiert <niljordan23@gmail.com>
2019-05-03 10:18:17 +0000

    [FIX] Fixed some things in the code of the file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv) - 1):
-        if (len(sys.argv) - 1 == 1):
+    if (len(sys.argv) > 1):
+        if (len(sys.argv) == 2):
             print("{:d} argument:".format(len(sys.argv) - 1))
         else:
             print("{:d} arguments:".format(len(sys.argv) - 1))
