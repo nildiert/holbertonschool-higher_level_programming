d098e9238007f6a49db92d60ae69ad934defc8b7
nildiert <niljordan23@gmail.com>
2019-05-03 10:33:22 +0000

    [FIX] Fixed a point in the format when the number of arguments are 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) == 2:
             print("{} argument:".format(len(sys.argv) - 1))
             print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
