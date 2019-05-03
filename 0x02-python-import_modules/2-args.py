11316f4b4ed1a82c23aff82876f985fedeadadd8
nildiert <niljordan23@gmail.com>
2019-05-03 10:20:22 +0000

    [FIX] Fixed the mode in that import the module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    if (len(sys.argv) > 1):
-        if (len(sys.argv) == 2):
-            print("{:d} argument:".format(len(sys.argv) - 1))
+    from sys import argv
+    if (len(argv) > 1):
+        if (len(argv) == 2):
+            print("{:d} argument:".format(len(argv) - 1))
         else:
-            print("{:d} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {}".format(i, sys.argv[i]))
+            print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {}".format(i, argv[i]))
     else:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
+        print("{:d} arguments:".format(len(argv) - 1))
