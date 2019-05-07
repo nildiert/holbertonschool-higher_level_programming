744f1ed2e21cc69774afbf7033d78819eb1e3d6e
nildiert <niljordan23@gmail.com>
2019-05-07 21:44:41 +0000

    [FIX] Fixed the None case

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = 0
-    if my_list is not None:
+    if my_list is None:
+        return None
+    else:
         my_list.sort(reverse=True)
         max = my_list[0]
         return (max)
-    return None
