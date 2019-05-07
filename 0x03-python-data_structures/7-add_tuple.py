ded14f69bdbe1a1664ae7fd6f580c3a59f1632d0
nildiert <niljordan23@gmail.com>
2019-05-07 20:01:59 +0000

    [FIX] Fixed tuples when both tuples are (1)

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,6 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
                 tuple_a = new_tuple
         if (tuple_b == ()):
                 tuple_b = new_tuple
+        if (tuple_a == (1) and tuple_b == (1)):
+                return (tuple_a, tuple_b)
         if len(tuple_a) is 1:
                 return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
         if len(tuple_b) is 1:
