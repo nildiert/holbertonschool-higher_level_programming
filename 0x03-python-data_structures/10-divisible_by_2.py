d65be38557817795aa12f32a0a971ae5ed98e9dc
nildiert <niljordan23@gmail.com>
2019-05-07 22:10:40 +0000

    [FIX] Fixed conditions in the new list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,8 +5,5 @@ def divisible_by_2(my_list=[]):
         return None
     else:
         for item in my_list:
-            if item % 2 == 0:
-                new_list.append(True)
-            else:
-                new_list.append(False)
+            new_list.append(item % 2 == 0)
     return (new_list)
