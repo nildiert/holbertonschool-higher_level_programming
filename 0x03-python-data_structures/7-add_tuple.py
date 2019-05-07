72054e89e98050a49512056a1ff7e1b0c91b999f
nildiert <niljordan23@gmail.com>
2019-05-07 19:50:34 +0000

    [FIX] Fixed the case when the tuple is Null

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,8 +8,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         if (tuple_b == ()):
                 tuple_b = new_tuple
         if len(tuple_a) is 1:
-                return (tuple_a[0] + 0, tuple_a[1] + tuple_b[1])
+                return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
         if len(tuple_b) is 1:
-                return (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+                return (tuple_a[0] + tuple_b[0], tuple_b[0] + 0)
         if len(tuple_a) >= 2 and len(tuple_b) >= 2:
                 return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
