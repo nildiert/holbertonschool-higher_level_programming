9e8bfdc14a43ec137efba51c6a3b5e0a7f65856c
nildiert <niljordan23@gmail.com>
2019-05-07 15:24:17 +0000

    [FEATURE] Added function that make copy of a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if (idx > 0 and idx < len(my_list)):
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return (new_list)
+    else:
+        return (my_list)
