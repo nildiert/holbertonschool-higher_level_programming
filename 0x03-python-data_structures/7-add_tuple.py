ebcc5a2c44753ec130f85a2e4260263758cb4acc
nildiert <niljordan23@gmail.com>
2019-05-07 19:56:46 +0000

    [FIX] Fixed tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
 
         new_tuple = (0, 0)
-        if (tuple_a == 1 and tuple_b):
-                return (tuple_a, tuple_b)
         if (tuple_a == ()):
                 tuple_a = new_tuple
         if (tuple_b == ()):
@@ -11,6 +9,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         if len(tuple_a) is 1:
                 return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
         if len(tuple_b) is 1:
-                return (tuple_a[0] + tuple_b[0], tuple_b[0] + 0)
+                return (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
         if len(tuple_a) >= 2 and len(tuple_b) >= 2:
                 return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
