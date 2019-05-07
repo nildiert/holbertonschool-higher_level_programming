31d1b00005d8e3e1407b75793cd569675a3cfba4
nildiert <niljordan23@gmail.com>
2019-05-07 19:34:56 +0000

    [FEATURE] Added function that makes tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+        new_tuple = (0, 0)
+
+        if (tuple_a == ()):
+                tuple_a = new_tuple
+        if (tuple_b == ()):
+                tuple_b = new_tuple
+        if len(tuple_a) is 1:
+                return (tuple_a[0] + 0, tuple_a[1] + tuple_b[1])
+        if len(tuple_b) is 1:
+                return (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+        if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+                return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
