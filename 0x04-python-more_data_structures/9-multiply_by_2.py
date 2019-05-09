759db053cc5ad60f4701c8dd2445778983e3309a
nildiert <niljordan23@gmail.com>
2019-05-09 23:41:26 +0000

    [FEATURE] ADded function that returns a new dictionary with the integers values mulltiplies for 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    keys, values = list(a_dictionary.keys()), list(a_dictionary.values())
+    return dict(zip(keys,map(lambda x: x * 2, values)))
