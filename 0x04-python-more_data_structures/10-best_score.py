08d9fe23f24d3447e71611e18f584bb2979fd625
nildiert <niljordan23@gmail.com>
2019-05-10 00:16:29 +0000

    [FIX] Fixed the case when the len of the diccionary is not 0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary.items())[0] if a_dictionary is not None else None
+    if a_dictionary is not None and len(a_dictionary) is not 0:
+        return max(a_dictionary.items())[0]
