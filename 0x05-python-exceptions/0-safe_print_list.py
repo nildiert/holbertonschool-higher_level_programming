fc35e88104b4ef0fac53bead0266560f031aa52a
nildiert <niljordan23@gmail.com>
2019-05-20 21:42:11 +0000

    [FIX] Fixed file 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+        item = 0
         for item in range(x):
                 try:
                         print("{0}".format(my_list[item]), end='')
