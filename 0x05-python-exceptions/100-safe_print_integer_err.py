30f5d2dedb192d0efe52e6930b679fe984727fca
nildiert <niljordan23@gmail.com>
2019-05-20 21:11:23 +0000

    [FEATURE] Added function 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
