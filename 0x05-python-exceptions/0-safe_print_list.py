18b0266e72e7d98913094308878e14cc20a95d55
nildiert <niljordan23@gmail.com>
2019-05-20 17:36:32 +0000

    [FEATURE] Added function print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+        for item in range(x):
+                try:
+                        print("{0}".format(my_list[item]), end='')
+                except IndexError:
+                        print("")
+                        return item
+        print("")
+        return item + 1
