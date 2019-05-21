755c036dcb6116c67767beb27ab004dc04024585
nildiert <niljordan23@gmail.com>
2019-05-21 21:06:51 +0000

    [FIX] Fixed the try exception in file 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 class Square:
     """ This is the Square class """
-    area = 0
-    def __init__(self, size=0, area=0):
+    def __init__(self, size=0):
         """Example of init method
         Args:
         size (int): Size of the square object
         """
-        try:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        except TypeError:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        except ValueError:
-            raise
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
         return self.__size * self.__size
