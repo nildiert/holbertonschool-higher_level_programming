20b4288d106309fc90ad9b8083ec21f609d1d3cc
nildiert <niljordan23@gmail.com>
2019-05-21 21:09:40 +0000

    [FIX] Fixed the pep8 style in file 8

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 class Square:
     """ This is the Square class """
-    area = 0
     def __init__(self, size=0):
         """Example of init method
         Args:
         size (int): Size of the square object
         """
         self.__size = size
-
 
     def area(self):
         """ Example of init method """
@@ -18,13 +16,14 @@ class Square:
     def size(self):
         """Example of init method"""
         return self.__size
+
     @size.setter
     def size(self, value):
         """Example of init method
         Args:
         size (int): Size of the square object
         """
-        if type(value) is not int :
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
