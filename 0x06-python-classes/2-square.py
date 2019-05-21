f8acd2180484b26b3c025c4c004555b6bab90388
nildiert <niljordan23@gmail.com>
2019-05-21 21:04:48 +0000

    [FIX] Fixed pep8 of file 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
         Args:
         size (int): Size of the square object
         """
-        if type(size) is not int :
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
