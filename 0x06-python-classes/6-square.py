ae5c55512754333e404d06eb36fa7fa4062aef42
nildiert <niljordan23@gmail.com>
2019-05-21 22:23:47 +0000

    [FIX] Fixed the file 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,11 +6,12 @@ class Square:
 
     """ This is the Square class """
 
-    def __init__(self, size=0, position(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
         """Example of init method
         Args:
         size (int): Size of the square object
         """
+        self.__position = position
         self.__size = size
 
     def area(self):
@@ -41,10 +42,23 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__position = value
+
     def my_print(self):
         """Print a square method"""
-        if self.__size is not 0:
-            for i in range(self.__size):
-                print("#" * self.__size)
-        else:
-            print("")
+        if self.__position[1] is not 0:
+            print("\n" * self.__position[1], end='')
+        for i in range(self.__size):
+            print("{}".format(" " * self.__position[0]), end='')
+            print("{}".format("#" * self.__size))
