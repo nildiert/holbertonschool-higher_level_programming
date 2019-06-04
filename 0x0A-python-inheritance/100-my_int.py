4e6f99b8d92253828aafc63529f45ff1b750018f
nildiert <niljordan23@gmail.com>
2019-06-04 23:53:42 +0000

    [FEATURE] Added class in file 100 that defines the comparisons  in inverse

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+class MyInt(int):
+
+    def __eq__(self, other):
+        if int(self) is other:
+            return False
+
+    def __ne__(self, other):
+        if int(self) is other:
+            return True
