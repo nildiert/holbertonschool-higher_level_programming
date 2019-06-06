cca15d0f7dd6c84cc03c434000f5f709e289798c
nildiert <niljordan23@gmail.com>
2019-06-06 02:05:46 +0000

    [FEATURE] Added file 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        my_dict = {}
+        if attrs is None:
+            return dict(self.__dict__)
+        else:
+            for key in attrs:
+                if key in self.__dict__:
+                    my_dict.update({key: self.__dict__[key]})
+
+            return my_dict
