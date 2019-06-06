9444a6c9750c60df5fff676aed3d3fa568308a7a
nildiert <niljordan23@gmail.com>
2019-06-06 02:41:41 +0000

    [FIX] Fixed file 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,7 +5,10 @@ def read_lines(filename="", nb_lines=0):
     size = 0
     with open(filename, encoding="utf-8") as myFile:
         if nb_lines <= 0:
-            print("{}".format(myFile.read()))
+            line = 'd'
+            while line != '':
+                line = myFile.readline()
+                print(line, end="")
         while size < nb_lines:
             line = myFile.readline()
             print(line, end="")
