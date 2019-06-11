1793079662e65ed29e844b325024e07149558a92
nildiert <niljordan23@gmail.com>
2019-06-11 21:04:18 +0000

    [FIX] Fixed function tojsonstring in base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Method to convert json to string """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         if list_dictionaries is not None:
-            if list_dictionaries is None:
-                return "[]"
             return json.dumps(list_dictionaries)
