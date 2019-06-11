7a2b62eec28c10bc55bbf93e509e38fa899e5117
nildiert <niljordan23@gmail.com>
2019-06-11 15:12:58 +0000

    [FEATURE] Added setter function

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
@@ -9,6 +10,11 @@ class Square(Rectangle):
     @property
     def size(self):
         return super().width
+
+    @size.setter
+    def size(self, value):
+        super(Square, self.__class__).width.fset(self, value)
+        super(Square, self.__class__).height.fset(self, value)
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
