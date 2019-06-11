a994c980d9f794695ee72d7d09ce2b2105440ab1
nildiert <niljordan23@gmail.com>
2019-06-11 17:25:04 +0000

    [FEATURE] Added function that returns the json representation of a dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
+import json
+
 
 class Base:
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
