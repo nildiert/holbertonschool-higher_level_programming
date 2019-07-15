62f8c82e03e3b2b4e7681634dd28f31e7539ed9e
nildiert <niljordan23@gmail.com>
2019-07-15 19:31:14 +0000

    [FEATURE] Added file 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- Query to order by temp
 -- This query shows by temp and city
-SELECT city, AVG(values) as avg_temp GROUP BY city ORDER BY DESC;
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
