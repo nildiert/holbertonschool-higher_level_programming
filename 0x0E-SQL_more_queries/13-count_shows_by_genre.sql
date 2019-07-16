1a20183b821248db2efc395ee5b9bc35c9cc8387
nildiert <niljordan23@gmail.com>
2019-07-16 18:07:27 +0000

    [FIX] Fixed sql style

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Query  to do a list with left joins
 -- This query lists movies with genre
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
        FROM tv_genres
        INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
        GROUP BY tv_genres.name
