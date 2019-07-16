09e50525a36c75d146c11da60905cd9e943abcca
nildiert <niljordan23@gmail.com>
2019-07-16 17:38:10 +0000

    [FIX] Fixed name of the first column

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Query  to do a list with left joins
 -- This query lists movies with genre
-SELECT tv_genres.name, COUNT(tv_genres.name) as number_of_shows
+SELECT tv_genres.name as genre, COUNT(tv_genres.name) as number_of_shows
        FROM tv_genres
        LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
        GROUP BY tv_genres.name
