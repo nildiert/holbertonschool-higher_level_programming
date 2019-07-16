b7d46f24c1be22c9fb680aceacfdce6ff5fba751
nildiert <niljordan23@gmail.com>
2019-07-16 17:49:46 +0000

    [FIX] Fixed file 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@
 SELECT tv_genres.name as genre, COUNT(tv_genres.name) as number_of_shows
        FROM tv_genres
        INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-       GROUP BY tv_genres.name
+       GROUP BY genre
        ORDER BY number_of_shows DESC
 ;
