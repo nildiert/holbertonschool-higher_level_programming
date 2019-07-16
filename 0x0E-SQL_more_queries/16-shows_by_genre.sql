cc0554fe2d9bf5e8c2ce35d16cafc72b9eba064c
nildiert <niljordan23@gmail.com>
2019-07-16 18:37:57 +0000

    [FEATURE] ADded file 15

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Query  to do a list with left joins
+-- This query lists movies with genre
+SELECT tv_shows.title, tv_genres.name
+       FROM tv_shows
+       LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+       LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+       ORDER BY tv_shows.title, tv_genres.name ASC;
