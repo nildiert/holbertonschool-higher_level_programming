547f75f7ba0b1faf07cfc8d858523418bdc4dfb4
nildiert <niljordan23@gmail.com>
2019-07-16 16:35:31 +0000

    [FIX] Fixed the default value in file 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- Query  to create table
 -- This query creates a table in a database passes like arg
 CREATE TABLE IF NOT EXISTS unique_id(
-       id INT UNIQUE,
+       id INT UNIQUE DEFAULT '1',
        name VARCHAR(256) NOT NULL
 );
