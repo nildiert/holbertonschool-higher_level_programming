7e13be4d4c9922ef7fbb5bcbeaebb6cf19b07885
nildiert <niljordan23@gmail.com>
2019-07-16 16:42:19 +0000

    [FIX] Fixed the order in the creation of table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- This query creates a table in a database passes like arg
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-       id INT UNIQUE NOT NULL AUTO_INCREMENT,
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY (id),
