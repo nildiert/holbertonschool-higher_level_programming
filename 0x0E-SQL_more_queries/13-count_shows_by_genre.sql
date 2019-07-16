e5a07986b7484048d53e2d9859a8e944ef7e5e60
nildiert <niljordan23@gmail.com>
2019-07-16 17:36:43 +0000

    [FEATURE] ADded file 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Query  to do a list with left joins
+-- This query lists movies with genre
+SELECT tv_genres.name, COUNT(tv_genres.name) as number_of_shows
+       FROM tv_genres
+       LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+       GROUP BY tv_genres.name
+       ORDER BY number_of_shows DESC
+;
