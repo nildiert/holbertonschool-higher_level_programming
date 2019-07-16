fe3c20affb6ed711586b93e19f9cffe7a0d9236b
nildiert <niljordan23@gmail.com>
2019-07-16 17:41:22 +0000

    [FIX] Fixed file 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- This query lists movies with genre
 SELECT tv_genres.name as genre, COUNT(tv_genres.name) as number_of_shows
        FROM tv_genres
-       RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+       INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
        GROUP BY tv_genres.name
        ORDER BY number_of_shows DESC
 ;
