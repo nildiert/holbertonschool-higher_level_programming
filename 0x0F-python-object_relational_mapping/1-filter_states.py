4483c2d32d92289344e085d132334e5eda2a18f6
nildiert <niljordan23@gmail.com>
2019-08-10 17:14:56 +0000

    [FIX] Fixed the exception of number of arguments

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,21 +2,24 @@
 # Script that lists all states that starging with N
 
 import MySQLdb
-from sys import argv
+import sys
 
 if len(argv) is 4:
-    db = MySQLdb.connect(
-        host='localhost',
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        port=3306
-    )
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
 
-    cursor = db.cursor()
+        cursor = db.cursor()
 
-    cursor.execute("SELECT id, name\
-    FROM states WHERE name LIKE 'N%'\
-    ORDER BY states.id ASC;")
-    for result in cursor:
-        print("{}".format(result))
+        cursor.execute("SELECT id, name\
+        FROM states WHERE name LIKE 'N%'\
+        ORDER BY states.id ASC;")
+        for result in cursor:
+            print("{}".format(result))
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
