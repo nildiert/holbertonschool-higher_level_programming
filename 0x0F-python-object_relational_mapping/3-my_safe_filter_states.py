61b1a3ef3b9ad242be7dce728c1420a1fabac83d
nildiert <niljordan23@gmail.com>
2019-08-11 21:28:51 +0000

    [FIX] Fixed the format in the print of the file 3 and the query

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,17 +3,24 @@
 import MySQLdb
 from sys import argv
 
-if len(argv) is 5:
-    db = MySQLdb.connect(
-        host='localhost',
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        port=3306
-    )
+if __name__ == '__main__':
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306
+        )
 
-    cursor = db.cursor()
-    rows = cursor.fetchall()
-    cursor.execute("SELECT id, name FROM states WHERE name='%s';" % argv[4])
-    for row in rows:
-        print("{}".format(row))
+        cursor = db.cursor()
+        rows = cursor.fetchall()
+        query = "SELECT id, name \
+        FROM states WHERE name='%s' \
+        ORDER BY states.id ASC;" % argv[4]
+        cursor.execute()
+        for row in rows:
+            print(row)
+        cursor.close()
+    except Exception:
+        pass
