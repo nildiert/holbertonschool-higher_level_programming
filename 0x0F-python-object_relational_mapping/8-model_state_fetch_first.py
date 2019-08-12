9daa6e02ea3fbe2d1714ca84f7ee41b672d56131
nildiert <niljordan23@gmail.com>
2019-08-12 04:08:45 +0000

    [FEATURE] Added file 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Query to show all the elements in the table """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Table, Column
+
+
+if __name__ == "__main__":
+    ''' Select * from state '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.bind = engine
+    Session = sessionmaker()
+    Session.bind = engine
+    current_session = Session()
+    data = current_session.query(State).first()
+    if data:
+        print("{}: {}".format(data.id, data.name))
+    else:
+        print("Nothing")
