1b9756e01d3dad140a4ed7bfdb7ecfb06a687e29
nildiert <niljordan23@gmail.com>
2019-08-12 09:52:03 +0000

    [FEATURE] Added file 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Query to show all the elements in the table """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Table, Column
+
+
+if __name__ == "__main__":
+    ''' Select * from state '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.bind = engine
+    Session = sessionmaker()
+    Session.bind = engine
+    current_session = Session()
+    result = current_session.query(State).filter(State.name.like('%a%')).all()
+    if result:
+        for row in result:
+            current_session.delete(row)
+        current_session.commit()
+    else:
+        pass
