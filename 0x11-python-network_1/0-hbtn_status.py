3d9ca48ce79db221050b41967c0768d74360b6d1
nildiert <niljordan23@gmail.com>
2019-08-26 16:53:51 +0000

    [FEATURE] Added file 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Script that fetches with a website
+
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf content: {}".format(html.decode('utf-8')))
