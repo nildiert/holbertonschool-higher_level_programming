57211107c53994d62db1b981d4fbb5873fe770b1
nildiert <niljordan23@gmail.com>
2019-08-26 23:10:43 +0000

    [FIX] Fixed file 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,7 @@
 """ This script takes a URL and displays the body of the response """
 import sys
 from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
+from urllib.error import HTTPError
 
 if __name__ == "__main__":
     try:
@@ -11,3 +11,5 @@ if __name__ == "__main__":
         print("{}".format(response.read().decode('utf-8')))
     except HTTPError as e:
         print("Error code: {}".format(e.code)
+    except:
+        pass
