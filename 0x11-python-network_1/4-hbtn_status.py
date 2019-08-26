345740aca58c03fb0f513aab379d4261c006920e
nildiert <niljordan23@gmail.com>
2019-08-26 23:33:55 +0000

    [FEATURE] Added file 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- type: {}".format(r.text))
+    print("\t- content: {}".format(r.text))
