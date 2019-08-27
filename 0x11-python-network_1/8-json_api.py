8acfc220d78cc5138a5df8a8601471b5a93fda42
nildiert <niljordan23@gmail.com>
2019-08-27 01:34:00 +0000

    [FIX] Fixed file 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+if __name__ == "__main__":
+    try:
+        import requests
+        import sys
+
+        url = "http://0.0.0.0:5000/search_user"
+        try:
+            q = {'q': sys.argv[1]}
+        except:
+            q = {'q': ""}
+        try:
+            r = requests.post(url, q)
+            if len(r.json()) == 0:
+                print("No result")
+            else:
+                print("[{}] {}".format(
+                    r.json().get('id'), r.json().get('name'))
+                )
+        except:
+            print("Not a valid JSON")
+
+    except Exception as err:
+        pass
