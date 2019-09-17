2598f8bd53c2021748dde470884ea5dbc982361a
nildiert <niljordan23@gmail.com>
2019-09-17 19:05:08 +0000

    [FEATURE] Fixed file 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,10 +4,14 @@
 */
 
 if (process.argv.length >= 3) {
-  const size = process.argv[2];
-  for (let i = 0; i < parseInt(size); i++) {
-    console.log('X'.repeat(size));
+  const size = process.argv[2]
+  if (!isNaN(size)) {
+    for (let i = 0; i < parseInt(size); i++) {
+      console.log('X'.repeat(size))
+    }
+  } else {
+    console.log('Missing size')
   }
 } else {
-  console.log('Missing size');
+  console.log('Missing size')
 }
