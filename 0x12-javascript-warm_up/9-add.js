53911eca9025c9be81e324aeb0f1a3ced3b8934c
nildiert <niljordan23@gmail.com>
2019-09-17 19:07:52 +0000

    [FIX] Fixed standard style in file 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@
   js
 */
 if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
-  console.log('NaN');
+  console.log('NaN')
 } else {
-  console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
+  console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]))
 }
