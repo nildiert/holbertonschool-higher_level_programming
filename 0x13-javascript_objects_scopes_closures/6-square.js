0bd40d4fd25be3deaf1c445ff222c07d0e63e6cd
nildiert <niljordan23@gmail.com>
2019-09-23 15:50:33 +0000

    [FEATURE] Added file 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const SquareBase = require('./5-square');
+class Square extends SquareBase {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
+}
+
+module.exports = Square;
