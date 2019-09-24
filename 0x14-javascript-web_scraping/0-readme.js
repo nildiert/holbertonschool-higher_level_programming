ecf1ce4063d23bde1d8cd40cdc97e1afab83d322
nildiert <niljordan23@gmail.com>
2019-09-24 17:14:22 +0000

    [FIX] Fixed file 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-console.log(fs.readFileSync(process.argv[2], 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
     console.log(data);
   }
-}));
+});
